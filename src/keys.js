e7caabad35fbc343279f44e17885672d3ded55e6
Vanessa <vanessa.cnr97@hotmail.com>
2020-02-13 17:15:45 -0600

    corre plis

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 module.exports = {
 
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'fieldservdb'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     }
 };
